b0b2bf9bacede49bfe4c76b6ded3e9a84d67b15d
EdwinRincon <rinconedwin@hotmail.com>
2020-02-09 21:52:49 +0100

    id iconos tipo envio - click evento SUBMIT

--- envios.js
+++ envios.js
@@ -1,18 +1,18 @@
 
 window.onload = traerNombre;
 var httpreq = new XMLHttpRequest();
-function traerNombre(){           
- cliente = localStorage.getItem('cliente')
- datosCliente = JSON.parse(cliente);
- document.getElementById("nombre").innerHTML = datosCliente.nombreCliente
- 
- 
- 
- httpreq.open('GET', 'http://localhost:8080/EjemploRestJDBC/webapi/destinatarios/'+datosCliente.idCliente)
- httpreq.onload = function () {
-     if (httpreq.readyState == 4) {
-         if (httpreq.status == 200) {
-          let datos = JSON.parse(this.responseText)
+function traerNombre() {
+    cliente = localStorage.getItem('cliente')
+    datosCliente = JSON.parse(cliente);
+    document.getElementById("nombre").innerHTML = datosCliente.nombreCliente
+
+
+
+    httpreq.open('GET', 'http://localhost:8080/EjemploRestJDBC/webapi/destinatarios/' + datosCliente.idCliente)
+    httpreq.onload = function () {
+        if (httpreq.readyState == 4) {
+            if (httpreq.status == 200) {
+                let datos = JSON.parse(this.responseText)
 
                 let res = document.querySelector('#dropdown');
                 res.innerHTML = '';
@@ -21,22 +21,32 @@ function traerNombre(){
                     res.innerHTML += `
                     <option>${item.nombreCliente}</option> `
                 }
-         } else {
-            /* Swal.fire({
+            } else {
+                /* Swal.fire({
+    
+                     title: 'Error',
+                     text:'La conexión al servidor falló.',
+                     icon:'question',
+                     onClose: () => {
+                         window.location.reload(true)
+                     }
+                 })*/
+            }
+        }
+    }
+    httpreq.send();
 
-                 title: 'Error',
-                 text:'La conexión al servidor falló.',
-                 icon:'question',
-                 onClose: () => {
-                     window.location.reload(true)
-                 }
-             })*/
-         }
-     }
- }
- httpreq.send();
- 
- 
- //localStorage.clear()
+
+    //localStorage.clear()
 
 }
+
+
+document.getElementById("submit").addEventListener("click", function () {
+
+    if (!document.getElementById('RPendiente').checked && !document.getElementById('ROficina').checked)
+    {
+        alert("chekea tus checks")
+    }
+
+});
