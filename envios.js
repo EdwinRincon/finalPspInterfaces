b8ffc9cdb4c33671ea3f1718e2dbc8a3d6622700
EdwinRincon <rinconedwin@hotmail.com>
2020-02-10 00:23:03 +0100

    Cargar Select Dinamicamente Arreglado, Rellenar Campos de Destinario al cambiar de Destinatario en el Select

--- envios.js
+++ envios.js
@@ -6,40 +6,52 @@ function traerNombre() {
     datosCliente = JSON.parse(cliente);
     document.getElementById("nombre").innerHTML = datosCliente.nombreCliente
 
-
-
     httpreq.open('GET', 'http://localhost:8080/EjemploRestJDBC/webapi/destinatarios/' + datosCliente.idCliente)
     httpreq.onload = function () {
         if (httpreq.readyState == 4) {
             if (httpreq.status == 200) {
-                let datos = JSON.parse(this.responseText)
-
-                let res = document.querySelector('#dropdown');
-                res.innerHTML = '';
-
-                for (let item of datos) {
-                    res.innerHTML += `
-                    <option>${item.nombreCliente}</option> `
+                let datosDestinatario = JSON.parse(this.responseText)
+                let select = document.querySelector("#select")
+                select.innerHTML = '';
+                select.innerHTML = `<option disabled required>Elegir Destinatario</option>`;
+                console.log(datosDestinatario);
+                for (let item of datosDestinatario) {
+                    
+                    
+                    select.innerHTML += `
+                    <option>${item.nombreDestinatario}</option> `
                 }
+                select.selectedIndex = "0";
+                
             } else {
-                /* Swal.fire({
-    
-                     title: 'Error',
-                     text:'La conexión al servidor falló.',
-                     icon:'question',
-                     onClose: () => {
-                         window.location.reload(true)
-                     }
-                 })*/
+            }
+        }
+    }
+    httpreq.send();
+    //localStorage.clear()
+}
+// A?adir a los campos de destinatario cuando cambias de destinatario en el Select
+document.getElementById("select").addEventListener("change", function() {
+    if(document.getElementById("select").value != "Elegir Destinatario")
+    {
+        var aux_nameDestinatario = document.getElementById("select").value
+
+    httpreq.open('GET', 'http://localhost:8080/EjemploRestJDBC/webapi/destinatarios/name/'+"\""+aux_nameDestinatario+"\"" )
+    httpreq.onload = function () {
+        if (httpreq.readyState == 4) {
+            if (httpreq.status == 200) {
+                let datosDestinatario = JSON.parse(this.responseText)
+                document.getElementById("dni").innerText = datosDestinatario.DNINIF;
+                document.getElementById("direccion").innerText = datosDestinatario.direccionCompleta;
+                document.getElementById("cp").innerText = datosDestinatario.codigoPostal;
             }
         }
     }
     httpreq.send();
 
 
-    //localStorage.clear()
-
-}
+    }
+});
 
 
 document.getElementById("submit").addEventListener("click", function () {
